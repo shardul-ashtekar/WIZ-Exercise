9893ae51d5e217edecd445d3a3a78e0cc77dad3a
Shardul Ashtekar <shardulashtekar@Sharduls-Laptop.local>
2025-11-07 03:52:21 +0530

    pushed the latest updated code vol.25

--- tf/modules/s3/main.tf
+++ tf/modules/s3/main.tf
@@ -2,34 +2,3 @@ resource "aws_s3_bucket" "mongo_db_backup" {
   bucket = var.bucket_name
   tags   = var.tags
 }
-
-resource "aws_s3_bucket_public_access_block" "mongo_db_backup_block" {
-  bucket                  = aws_s3_bucket.mongo_db_backup.id
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_policy" "mongo_db_backup_policy" {
-  bucket = aws_s3_bucket.mongo_db_backup.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "PublicReadList"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = [
-          "s3:GetObject",
-          "s3:ListBucket"
-        ]
-        Resource = [
-          "arn:aws:s3:::${aws_s3_bucket.mongo_db_backup.id}",
-          "arn:aws:s3:::${aws_s3_bucket.mongo_db_backup.id}/*"
-        ]
-      }
-    ]
-  })
-}

513341b5dfef65feec3737c4fc4a19e14c9f3348
Shardul Ashtekar <shardulashtekar@Sharduls-Laptop.local>
2025-11-06 20:08:12 +0530

    pushed the latest updated code vol.04

--- tf/modules/s3/main.tf
+++ tf/modules/s3/main.tf
@@ -3,11 +3,3 @@ resource "aws_s3_bucket" "db_backup" {
   tags   = var.tags
 }
 
-resource "aws_s3_bucket_public_access_block" "public" {
-  bucket                  = aws_s3_bucket.db_backup.id
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-

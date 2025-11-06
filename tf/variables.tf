48fa4b91ca7a6cbd1a7110de801b6598496adfa4
Shardul Ashtekar <shardulashtekar@Sharduls-Laptop.local>
2025-11-06 19:03:47 +0530

    reconfigured the variables

---
+++ tf/variables.tf
@@ -0,0 +1,67 @@
+
+############### AWS Variables ##################
+
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+}
+
+
+############### VPC Variables ##################
+
+variable "public_subnets" {
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
+}
+variable "private_subnets" {
+  description = "List of private subnet CIDR blocks"
+  type        = list(string)
+}
+variable "name_prefix" {
+  description = "Prefix for naming resources"
+  type        = string
+}
+
+
+############### EC2 Variables ##################
+
+variable "outdated_ami_id" {
+  description = "The AMI ID to use for the EC2 instances"
+  type        = string
+}
+variable "key_name" {
+  description = "The name of the key pair to use for EC2 instances"
+  type        = string
+}
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+}
+variable "default_instance_type" {
+  description = "The default instance type for EC2 instances"
+  type        = string
+}
+variable "common_tags" {
+  description = "Common tags to apply to all resources"
+  type        = map(string)
+}
+variable "db_backup_bucket" {
+  description = "The S3 bucket name for database backups"
+  type        = string
+}
+
+
+############### EKS Variables ##################
+
+variable "cluster_name" {
+  description = "The name of the EKS cluster"
+  type        = string
+}
+variable "eks_cluster_version" {
+  description = "The version of EKS to use"
+  type        = string
+}
+variable "eks_node_ami" {
+  description = "The AMI ID for the EKS worker nodes"
+  type        = string
+}

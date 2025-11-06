ea286bd96698f90501ad4c8b73db5b22041ae4af
Shardul Ashtekar <shardulashtekar@Sharduls-Laptop.local>
2025-11-07 01:09:19 +0530

    pushed the latest updated code vol.16

--- tf/modules/eks/main.tf
+++ tf/modules/eks/main.tf
@@ -181,7 +181,7 @@ resource "aws_eks_addon" "vpc_cni" {
 resource "aws_eks_addon" "coredns" {
   cluster_name      = aws_eks_cluster.wiz-eks.name
   addon_name        = "coredns"
-  addon_version     = "v1.10.1-eksbuild.1"
+  addon_version     = "v1.11.4-eksbuild.2"
 
   tags = merge(var.tags, {
     Name = "${var.cluster_name}-coredns-addon"
@@ -191,9 +191,20 @@ resource "aws_eks_addon" "coredns" {
 resource "aws_eks_addon" "kube_proxy" {
   cluster_name      = aws_eks_cluster.wiz-eks.name
   addon_name        = "kube-proxy"
-  addon_version     = "v1.32.0-eksbuild.1"
+  addon_version     = "v1.32.6-eksbuild.12"
 
   tags = merge(var.tags, {
     Name = "${var.cluster_name}-kube-proxy-addon"
   })
+}
+
+resource "aws_eks_addon" "amazon_ebs_csi_driver" {
+  cluster_name      = aws_eks_cluster.wiz-eks.name
+  addon_name        = "aws-ebs-csi-driver"
+  addon_version     = "v1.51.1-eksbuild.1"
+
+  tags = merge(var.tags, {
+    Name = "${var.cluster_name}-ebs-csi-driver-addon"
+  })
+
 }
